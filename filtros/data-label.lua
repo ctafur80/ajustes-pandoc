63227461247ea3e94a6b252e1ced97606663767c
ctafur80 <ctafur@gmail.com>
2025-08-27 10:05:27 +0200

    Modificado filtro.

--- filtros/data-label.lua
+++ filtros/data-label.lua
@@ -2,36 +2,48 @@
 
 
 
-function Div(div)
-
-  -- Proof environment
-  if div.class == "proof" then
-    if div.content[1] then
-      div.content[1].content:insert(1, pandoc.Strong({pandoc.Emph(pandoc.Str("Demostración.— "))}))
+-- Función auxiliar para comprobar si un elemento tiene una clase específica
+function has_class(element, class_name)
+  if not element.attr or not element.attr.classes then
+    return false
+  end
+  for _, c in ipairs(element.attr.classes) do
+    if c == class_name then
+      return true
     end
   end
-
-
-  -- `data-label` attribute
-  local label = div.attr.attributes['data-label']
-  if label and label ~= "" then
-    -- Crea el elemento en negrita e itálica, añadiendo dos puntos y un espacio
-    local new_inline = pandoc.Strong({pandoc.Emph(pandoc.Str(label .. ".— "))})
-
-    -- Comprueba si hay contenido y si el primer bloque es un párrafo
-    if div.content[1] and div.content[1].t == "Para" then
-      -- Inserta el nuevo elemento inline al principio del contenido del párrafo
-      div.content[1].content:insert(1, new_inline)
-
-      -- Elimina el atributo para que no se renderice
-      div.attr.attributes['data-label'] = nil
-    end
-  end
-
-
-
-  return div
+  return false
 end
 
 
 
+
+function Div(div)
+
+  local sep = ".— "
+
+  -- `proof` class.
+  if has_class(div, "proof") then
+    local title = "Demostración"
+    local str_to_insert = pandoc.Strong(pandoc.Emph(pandoc.Str(title .. sep)))
+
+    if div.content[1] then
+      div.content[1].content:insert(1, str_to_insert)
+    end
+  end
+
+
+  -- `data-label` attribute.
+  local label = div.attr.attributes['data-label']
+  if label and label ~= "" then
+    local str_to_insert = pandoc.Strong({pandoc.Emph(pandoc.Str(label .. sep))})
+
+    -- if div.content[1] and div.content[1].t == "Para" then
+    if div.content[1] then
+      div.content[1].content:insert(1, str_to_insert)
+      div.attr.attributes['data-label'] = nil
+    end
+  end
+
+  return div
+end
